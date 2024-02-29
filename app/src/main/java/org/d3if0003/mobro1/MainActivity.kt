c5ec3fe18369347387828c4f865aa009c5204336
Haidar341 <haizak3@gmail.com>
2024-02-29 18:11:53 +0700

    Commit Task 3.1 Praktikum2

--- app/src/main/java/org/d3if0003/mobro1/MainActivity.kt
+++ app/src/main/java/org/d3if0003/mobro1/MainActivity.kt
@@ -1,14 +1,21 @@
 package org.d3if0003.mobro1
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if0003.mobro1.ui.theme.Mobro1Theme
 
@@ -26,18 +33,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!\nNim: 670621009\nKelas : 45-03",
-            modifier = modifier
-    )
+fun MainScreen(content : @Composable (Modifier) -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ){ padding ->
+        content(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    MainScreen {Modifier ->
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier
+        )
+
+    }
+
 }
 
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground =  true)
 @Composable
-fun GreetingPreview() {
+fun ScreenPreview() {
     Mobro1Theme {
-        Greeting("Haidar Jindan Ahnif")
+        Greeting("Android")
     }
 }
